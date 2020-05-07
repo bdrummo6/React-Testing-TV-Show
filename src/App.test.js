fe1d406a23e93581cda4b4674b186dc3cc2a6a6d
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-05-06 21:21:09 -0400

    Update App.test.js
    Cleaned up code

--- src/App.test.js
+++ src/App.test.js
@@ -15,12 +15,12 @@ test('App renders properly when the user selects Season 1 in the dropdown ', asy
 
 	// wait for the data to be resolved
 	await waitFor(() =>
-		queryAllByTestId( /season/i)
+		queryAllByTestId(/season/i)
 	);
 
 	// Act: Imitating if the user clicks on the dropdown menu and then 'Season 1' in the menu
-	userEvent.click(getByText( /select a season/i));
-	userEvent.click(getByText( /season 1/i));
+	userEvent.click(getByText(/select a season/i));
+	userEvent.click(getByText(/season 1/i));
 
 	// Assert: Check if the 8 episodes in 'Season 1' are rendered
 	expect(queryAllByText(/episode/i)).toHaveLength(8);
@@ -34,12 +34,12 @@ test('App renders properly when the user selects Season 2 in the dropdown ', asy
 
 	// wait for the data to be resolved
 	await waitFor(() =>
-		queryAllByTestId( /season/i)
+		queryAllByTestId(/season/i)
 	);
 
 	// Act: Imitating if the user clicks on the dropdown menu and then 'Season 2' in the menu
-	userEvent.click(getByText( /select a season/i));
-	userEvent.click(getByText( /season 2/i));
+	userEvent.click(getByText(/select a season/i));
+	userEvent.click(getByText(/season 2/i));
 
 	// Assert: Check if the 9 episodes in 'Season 2' are rendered
 	expect(queryAllByText(/episode/i)).toHaveLength(9);
@@ -53,12 +53,12 @@ test('App renders properly when the user selects Season 3 in the dropdown ', asy
 
 	// wait for the data to be resolved
 	await waitFor(() =>
-		queryAllByTestId( /season/i)
+		queryAllByTestId(/season/i)
 	);
 
 	// Act: Imitating if the user clicks on the dropdown menu and then 'Season 3' in the menu
-	userEvent.click(getByText( /select a season/i));
-	userEvent.click(getByText( /season 3/i));
+	userEvent.click(getByText(/select a season/i));
+	userEvent.click(getByText(/season 3/i));
 
 	// Assert: Check if the 8 episodes in 'Season 3' are rendered
 	expect(queryAllByText(/episode/i)).toHaveLength(8);
@@ -72,12 +72,12 @@ test('App renders properly when the user selects Season 4 in the dropdown ', asy
 
 	// wait for the data to be resolved
 	await waitFor(() =>
-		queryAllByTestId( /season/i)
+		queryAllByTestId(/season/i)
 	);
 
 	// Act: Imitating if the user clicks on the dropdown menu and then 'Season 4' in the menu
-	userEvent.click(getByText( /select a season/i));
-	userEvent.click(getByText( /season 4/i));
+	userEvent.click(getByText(/select a season/i));
+	userEvent.click(getByText(/season 4/i));
 
 	// Assert: Check if the 1 episode in 'Season 4' are rendered
 	expect(queryAllByText(/episode/i)).toHaveLength(1);
