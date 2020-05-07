a6e18f42f3969362c2dfab02250e3b443a701f29
bdrummo6 <bradley.drummonds@gmail.com>
2020-05-06 20:14:13 -0400

    Completed Episodes.test.js
    Passed mockData into Episodes which is an array of 8 episode objects, so I wrote an assertion that expects there to be 8 rendered episodes.

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Episodes from './Episodes';
+import mockEpisodeData from '../mockData/mockEpisodeData';
 
 // Test the Episodes components as the props change
 test('Episodes renders properly when props changes', () => {
 	// Initial render of Episodes component passing it an empty array via props
-	const { queryAllByTestId } = render(
+	const { queryAllByTestId, rerender } = render(
 		<Episodes episodes={[]} />
 	);
 
@@ -14,4 +15,9 @@ test('Episodes renders properly when props changes', () => {
 	expect(queryAllByTestId(/episode/i)).toStrictEqual([]); // Assertion that an empty array was passed to Episodes
 	expect(queryAllByTestId(/episode/i)).toHaveLength(0); // Assertion that no episodes have been rendered
 
+	// Rerender Episodes with the episode data from the mockEpisodeData array
+	rerender(<Episodes episodes={mockEpisodeData} />);
+
+	// Assertion that all 8 episodes in season 1 from the mockEpisodeData array rendered
+	expect(queryAllByTestId(/episode/i)).toHaveLength(8);
 });
