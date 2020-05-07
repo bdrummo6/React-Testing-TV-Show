093dc13f7bea3329fe1a88954e6b68226ee3bf95
bdrummo6 <bradley.drummonds@gmail.com>
2020-05-06 20:01:10 -0400

    Update Episodes.test.js
    Test  where as passed an empty array as props to Episodes and then made two assertions about Episodes.

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 
+import Episodes from './Episodes';
+
+// Test the Episodes components as the props change
+test('Episodes renders properly when props changes', () => {
+	// Initial render of Episodes component passing it an empty array via props
+	const { queryAllByTestId } = render(
+		<Episodes episodes={[]} />
+	);
+
+	// Assertions
+	expect(queryAllByTestId(/episode/i)).toStrictEqual([]); // Assertion that an empty array was passed to Episodes
+	expect(queryAllByTestId(/episode/i)).toHaveLength(0); // Assertion that no episodes have been rendered
+
+});
